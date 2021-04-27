eb186440ec98027f127b612cd6b3d29607ff912c
whkessler <82409360+whkessler@users.noreply.github.com>
2021-04-27 18:29:05 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function counter(){
+
+    var likescount =document.querySelector('#likes');
+    likes.innerHTML = parseInt(likescount.textContent)+1;
+}
+function counter1(){
+
+    var likescount1 =document.querySelector('#likes1');
+    likes1.innerHTML = parseInt(likescount1.textContent)+1;
+}function counter2(){
+
+    var likescount2 =document.querySelector('#likes2');
+    likes2.innerHTML = parseInt(likescount2.textContent)+1;
+}
